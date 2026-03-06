7135400cd09546cedb4a81a992e071d5849429c9
Tiến Sĩ <tahoangphuc111@gmail.com>
2026-03-06 19:09:57 +0700

    update __init__.py

--- judge/contest_format/__init__.py
+++ judge/contest_format/__init__.py
@@ -1,5 +1,4 @@
 from judge.contest_format.atcoder import AtCoderContestFormat
-from judge.contest_format.codeforces import CodeforcesContestFormat
 from judge.contest_format.default import DefaultContestFormat
 from judge.contest_format.ecoo import ECOOContestFormat
 from judge.contest_format.icpc import ICPCContestFormat
