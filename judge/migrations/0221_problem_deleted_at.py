1bf68d7c7a2155680b79f3d1fab9609e17d2da22
Đinh Cao Minh Quân <71386467+kitsunehivern@users.noreply.github.com>
2026-04-08 06:47:42 +0000

    fix migration

--- judge/migrations/0221_problem_deleted_at.py
+++ judge/migrations/0221_problem_deleted_at.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('judge', '0219_problemdata_zipfile_size_alter_contest_authors_and_more'),
+        ('judge', '0220_alter_problem_options_problemtestcase_is_example_and_more'),
     ]
 
     operations = [
